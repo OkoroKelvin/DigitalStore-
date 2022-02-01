f68e8dc6c18fdcae46ef7cc7a2eba7b687d84287
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-02-02 00:30:26 +0100

    Read and write to a text file

--- src/file/WritingFiles.java
+++ src/file/WritingFiles.java
@@ -8,9 +8,12 @@ import java.nio.file.Path;
 public class WritingFiles {
     public static void main(String[] args) throws IOException {
         Path path = Path.of("Resources/write.txt");
-        BufferedWriter writer = Files.newBufferedWriter(path);
-        writer.write("Hello World");
-        writer.close();
+        try(BufferedWriter writer = Files.newBufferedWriter(path);){
+            writer.write("Hello World, This is the journey to the future of Java ");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
         System.out.println("Done!");
     }
 }
